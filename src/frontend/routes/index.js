4ac9747b733b58a6e75e76bda06a0d7427926dd3
Brahyam Meneses <meneses.brahyam@gmail.com>
2017-03-13 22:33:14 -0400

    login/signup working using forms instead of feathers client

--- src/frontend/routes/index.js
+++ src/frontend/routes/index.js
@@ -4,12 +4,32 @@ const router = express.Router();
 module.exports = function (app) {
 
   router.get('/', function (req, res, next) {
-    res.render('landing');
+    res.redirect('/ui/dashboard');
+  });
+
+  router.get('/ui/dashboard', function (req, res, next) {
+    res.render('dashboard');
   });
 
   router.get('/login', function (req, res, next) {
     res.render('login');
   });
+
+  router.get('/signup', function (req, res, next) {
+    res.render('signUp');
+  });
+
+  router.post('/signup', function (req, res, next) {
+    const body = req.body;
+    app.service('users').create({
+      email: body.email,
+      password: body.password
+    })
+      .then(user => res.redirect('/login'))
+      .catch(next);
+  });
+
+
 
   return router;
 };
--- src/services/authentication/index.js
+++ src/services/authentication/index.js
@@ -8,7 +8,22 @@ const GoogleTokenStrategy = require('passport-google-token').Strategy;
 module.exports = function () {
   const app = this;
 
-  let config = app.get('auth');
+  let config = {
+    "token": {
+      "secret": "GYUH62cUzLwHgpeOtV44i06CmxmlDr2YJbCz+r6+GSvpJ3hSrpJ/VNvPml8/oYahGjv6OgJ4S5FxMPt9h0zxpA=="
+    },
+    "local": {},
+    "google": {
+      "clientID": "your google client id",
+      "clientSecret": "your google client secret",
+      "permissions": {
+        "scope": [
+          "profile"
+        ]
+      }
+    },
+    "successRedirect": "/ui/dashboard"
+  };
 
   config.google.strategy = GoogleStrategy;
   config.google.tokenStrategy = GoogleTokenStrategy;
